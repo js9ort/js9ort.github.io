02764eda90964573b22c40090c55a479a6c9980d
User <test@test.com>
2019-06-04 10:06:29 +0300

    Upload

---
+++ precache-manifest.1b74d8a78303cd66f5ae3a85d426c464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc6b115e6dca4ac9496b",
+    "url": "/static/css/main.dc53eca9.chunk.css"
+  },
+  {
+    "revision": "dc6b115e6dca4ac9496b",
+    "url": "/static/js/main.dc6b115e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "55292f0d2a9e5ac23c69",
+    "url": "/static/js/2.55292f0d.chunk.js"
+  },
+  {
+    "revision": "d4fb690f6d695032fd8e3d3b77330171",
+    "url": "/index.html"
+  }
+];
